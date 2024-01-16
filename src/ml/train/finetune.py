c97f7adbac4623bdb5edba6f9e91448b0009d638
Kilian Sprenkamp <giqsubmission@gmail.com>
2024-01-16 15:34:13 +0100

    Initial commit

---
+++ src/ml/train/finetune.py
@@ -0,0 +1,45 @@
+from datasets import load_dataset
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+train_dataset = load_dataset('json', data_files='notes.jsonl', split='train')
+eval_dataset = load_dataset('json', data_files='notes_validation.jsonl', split='train')
+
+class Finetune:
+    def __init__(self, model_config_path) -> None:
+        self.model_config = self.load_config(model_config_path)
+        self.base_model_id = model_config_path['base_model_id']
+        self.data_path = model_config_path['data_path']
+        self.model, self.tokenizer = self.load_model_and_tokenizer()
+
+    def load_config(self, model_config_path):
+        with open(model_config_path, 'r') as f:
+            model_config = json.load(f)
+        return model_config
+
+    def load_model_and_tokenizer(self):
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16
+        )
+        model = AutoModelForCausalLM.from_pretrained(self.base_model_id,
+                                                      quantization_config=bnb_config) 
+
+        tokenizer = AutoTokenizer.from_pretrained(
+            self.base_model_id,
+            padding_side="left",
+            add_eos_token=True,
+            add_bos_token=True,
+        )
+        tokenizer.pad_token = tokenizer.eos_token
+
+        return model, tokenizer
+    
+    def load_and_preprocess(self):
+        train_dataset = load_dataset('json', data_files='notes.jsonl', split='train')
+        val_dataset = load_dataset('json', data_files='notes_validation.jsonl', split='train')
+
+
+    
---
+++ src/scrape/oldp.py
@@ -0,0 +1,16 @@
+import oldp_client 
+
+conf = oldp_client.Configuration()
+
+conf.api_key['Authorization'] = '73efd304f9ecb4d715768ca19523e87f86c8ec83'  # TODO Replace this with your API key
+conf.api_key_prefix['Authorization'] = 'Token'
+
+api_client = oldp_client.ApiClient(conf)
+
+laws_api = oldp_client.LawsApi(api_client)
+
+#courts = courts_api.courts_list().results
+thread = laws_api.laws_list(async_req=True)
+result = thread.get()
+
+print(len(result))
