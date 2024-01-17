e8a8665bf708c2351b38cc0faee4faa288c9422d
sprenkamp <kilian@sprenkamp.de>
2024-01-17 14:56:32 +0000

    add eval logic

--- src/ml/evaluation/eval.py
+++ src/ml/evaluation/eval.py
@@ -1,11 +1,13 @@
 import json
-import yaml 
+import yaml
+import csv
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain.chat_models import ChatOpenAI
 
 
 
-class Finetune:
+class Evaluation:
     def __init__(self, model_config_path) -> None:
         self.model_config = self.load_config(model_config_path)
 
@@ -14,22 +16,42 @@ class Finetune:
             model_config = yaml.load(f, Loader=yaml.FullLoader)
         return model_config
 
-    def load_model_and_tokenizer_hf(self):
-        print(self.model_config["repo_id"])
-        tokenizer = AutoTokenizer.from_pretrained(self.model_config["repo_id"])
-        model = AutoModelForCausalLM.from_pretrained(self.model_config["repo_id"])
-        pipe = pipeline("text-generation", 
-               model=model, 
-               tokenizer=tokenizer, 
-               max_new_tokens=512
-               )
+    def load_model(self):
+        print("using", self.model_config["repo_id"], "for evaluation")
+        if "gpt" in self.model_config["repo_id"]:
+            self.llm = ChatOpenAI(
+            temperature=0,  # Make as deterministic as possible
+            model_name=self.model_config['model_name'],
+        )
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(self.model_config["repo_id"])
+            model = AutoModelForCausalLM.from_pretrained(self.model_config["repo_id"])
+            pipe = pipeline("text-generation", 
+                model=model, 
+                tokenizer=tokenizer, 
+                max_new_tokens=512
+                )
+            self.llm = HuggingFacePipeline(pipeline=pipe)
 
-        self.llm = HuggingFacePipeline(pipeline=pipe)
-
-
+    def eval(self):
+        splits = self.model_config["repo_id"]
+        if len(splits) > 1:
+            model_name = splits[1]
+        else:
+            model_name = splits[0]
+        out_path = self.model_config["eval_result_path"] + "_" + model_name + "_" + self.model_config["eval_path"].split("/")[-1].replace(".txt", ".csv")
+        with open(self.model_config["eval_path"], 'r') as input_file, open(out_path, 'w', newline='') as output_file:
+            csv_writer = csv.writer(output_file)
+            csv_writer.writerow(["Question", "Answer"])  # Writing the header row
+            for line in input_file:
+                line = line.strip()  # Remove any leading/trailing whitespace
+                if line:
+                    answer = self.llm(line)
+                    csv_writer.writerow([line, answer[0]["generated_text"]])
 
 if __name__ == "__main__":
     model_config_path = "src/ml/config/bloke_german.yaml"
-    finetune = Finetune(model_config_path)
-    finetune.load_model_and_tokenizer_hf()
-    finetune.llm("Hello world")
+    model_config_path = "src/ml/config/bloke_german.yaml"
+    evaluation = Evaluation(model_config_path)
+    evaluation.load_model()
+    evaluation.eval()
