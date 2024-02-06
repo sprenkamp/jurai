dda20882ee89b09201d42dd44778f32878af9395
Kilian Sprenkamp <kilian@sprenkamp.de>
2024-02-06 22:48:58 +0100

    debug

--- src/ml/train/finetune.py
+++ src/ml/train/finetune.py
@@ -140,7 +140,7 @@ class Finetune:
                 eval_steps=25,               # Evaluate and save checkpoints every 50 steps
                 do_eval = True if self.val_dataset is not None else False,
                 report_to = self.model_config["wandb"] if self.model_config["wandb"] else False,
-                run_name = f"{self.model_config['wandb']}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}" if self.model_config["wandb"] else None
+                run_name = f"{self.model_config['wandb']}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}" if self.model_config["wandb"] else None,
                 push_to_hub=self.model_config["push_to_hub"],
             ),
             data_collator=transformers.DataCollatorForLanguageModeling(self.tokenizer, mlm=False),
